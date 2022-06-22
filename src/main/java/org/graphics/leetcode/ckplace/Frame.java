c373dd6a166682cca6d22548c26449b096a8734d
James Green <github@ckplace.org>
2022-06-21 19:08:55 -0600

    Completed Problem One
    Signed-off-by: James Green <github@ckplace.org>

---
+++ src/main/java/org/graphics/leetcode/ckplace/Frame.java
@@ -0,0 +1,10 @@
+package org.graphics.leetcode.ckplace;
+
+import javax.swing.*;
+
+public class Frame {
+    private JComboBox comboBox1;
+    private JButton showStatsButton;
+    private JButton navigateButton;
+    private JComboBox comboBox2;
+}
